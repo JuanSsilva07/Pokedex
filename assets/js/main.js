04bd15395b67fa1e97f9ea3e233edf46a6eab6d0
Juan Soares Silva <70165034+JuanSsilva07@users.noreply.github.com>
2022-09-09 14:52:14 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+const maxRecords = 248
+const limit = 8
+let offset = 0;
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+            <div class = "more">
+                <span class="info experienceBase"> XP Base: ${pokemon.experienceBase}</span>
+                <span class="info height"> Height: ${pokemon.height} M</span>
+                <span class="info weight"> Weight: ${pokemon.weight} KG</span>
+            </div>
+        </li>
+    `
+}
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNexPage = offset + limit
+
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
---
+++ assets/js/pokemon-model.js
@@ -0,0 +1,11 @@
+
+class Pokemon {
+    number;
+    name;
+    type;
+    types = [];
+    experienceBase;
+    heigth;
+    weight;
+    photo;
+}
